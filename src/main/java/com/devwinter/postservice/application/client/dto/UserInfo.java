b8458f3b13ed3517ea1910697cd85887c90b7125
donghun93 <alswn4516@gmail.com>
2023-01-28 16:48:07 +0900

    Feat: "추가 게시글 작성 application 계층 개발"
    Feign Client로 외부 API(user-service)와 연동
    게시글 작성 application 계층 개발
    
    Resolvers: #4

---
+++ src/main/java/com/devwinter/postservice/application/client/dto/UserInfo.java
@@ -0,0 +1,11 @@
+package com.devwinter.postservice.application.client.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserInfo {
+    private Long memberId;
+    private String email;
+}
